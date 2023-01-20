b9355d7706d229cd0dbf5d8c27f7b1149c8a24aa
Ramis <Sir.ramis@mail.ru>
2023-01-20 14:31:40 +0300

    izmenil7

--- pi/pi/urls.py
+++ pi/pi/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('we/', include('we.urls')),
-    path('', include('mainApp.urls')),
+    path('contact', include('mainApp.urls')),
 ]
